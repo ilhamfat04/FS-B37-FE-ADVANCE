2ac64cb1c403db45ff2cbeb81d9234f056f61efb
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-01 13:52:34 +0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,35 @@
 // create style here
+const styles = {
+  bgColor: {
+    backgroundColor: "blue"
+  },
+  fontColor: {
+    color: "white"
+  }
+}
 
 // create Form component
 function Form() {
   return (
-    // code inside div
-    <div>
-      {/* form */}
-    </div>
+    <form style={{ backgroundColor: "red" }}>
+      <div >
+        <label htmlFor="username" style={styles.fontColor}>
+          Username
+        </label>
+        <input
+          id="username"
+          placeholder="Input username"
+          style={styles.bgColor}
+        />
+        <Button />
+      </div>
+    </form>
+  );
+}
+
+function Button() {
+  return (
+    <button style={styles.fontColor}>Click Me</button>
   )
 }
 
